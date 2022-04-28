1aab9790f22e60e0f9109ac05dee379950c8cdda
Wille Eriksson <wille.eriksson96@gmail.com>
2022-04-28 15:50:48 +0200

    Update setup files

--- setup.py
+++ setup.py
@@ -1,5 +1,19 @@
+import pathlib
+import pkg_resources
 from setuptools import setup, find_packages
+
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
+
 setup(
     name='rl_instruments',
-    packages=find_packages()
+    description='Reinforcement Learning for Musical Instrument Models Exploration',
+    author='Wille Eriksson',
+    author_email='wille.eriksson96@gmail.com',
+    packages=find_packages(),
+    install_requires=install_requires
 )
