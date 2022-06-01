bbced31de630b5932f976999721e6929860d1705
Wille Eriksson <wille.eriksson96@gmail.com>
2022-06-01 10:53:15 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
-import pathlib
-import pkg_resources
 from setuptools import setup, find_packages
-
-with pathlib.Path('requirements.txt').open() as requirements_txt:
-    install_requires = [
-        str(requirement)
-        for requirement
-        in pkg_resources.parse_requirements(requirements_txt)
-    ]
 
 setup(
     name='rl_instruments',
@@ -15,5 +6,4 @@ setup(
     author='Wille Eriksson',
     author_email='wille.eriksson96@gmail.com',
     packages=find_packages(),
-    install_requires=install_requires
 )
